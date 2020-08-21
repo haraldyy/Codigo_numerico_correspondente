93811bbd4faddc3cc6004a54a4caab59c3e42eb4
haraldyy <69537970+haraldyy@users.noreply.github.com>
2020-08-21 17:36:20 -0300

    Add files via upload

---
+++ Qual caractere unicode com interface.py
@@ -0,0 +1,12 @@
+def quantidade():
+    q = input('Digite um caractere:')
+    
+    print(ord(q))
+
+
+
+def main():
+    quantidade()
+
+if __name__ =='__main__':
+    main()
---
+++ Qual caractere unicode.py
@@ -0,0 +1,12 @@
+def quantidade():
+    q = input()
+    
+    print(ord(q))
+
+
+
+def main():
+    quantidade()
+
+if __name__ =='__main__':
+    main()
